14102cc92b531d4db7bee980827664ac6ff973d8
Joey727 <joshuasackeyjr.1124@gmail.com>
2024-12-17 12:24:37 +0000

    using the toString method to print out an array of integers

---
+++ Calculation.java
@@ -0,0 +1,11 @@
+
+import java.util.Arrays;
+
+
+public class Calculation {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 5, 7, 9};
+        System.out.print(Arrays.toString(nums));
+        
+    }
+}
