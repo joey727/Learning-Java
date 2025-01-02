c8bbe427d48c6da2e106b7855066629587ac88e6
Joey727 <joshuasackeyjr.1124@gmail.com>
2025-01-02 13:46:16 +0000

    trying out working with LinkedList

---
+++ LinkedListDemo.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<Integer> lst = new LinkedList<>();
+        Scanner scan = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            System.out.println("Enter a number (or type 'exit' to quit): ");
+            input = scan.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+            try {
+                int number = Integer.parseInt(input);
+                lst.add(number);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        System.out.println("Final list: " + lst);
+        scan.close();
+    }
+}
