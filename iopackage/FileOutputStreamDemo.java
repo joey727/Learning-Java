2f396712d3e2daeb6c94fb37aa2f473872a52aed
Joey727 <joshuasackeyjr.1124@gmail.com>
2025-01-03 17:01:26 +0000

    Learning about writing to a file

---
+++ iopackage/FileOutputStreamDemo.java
@@ -0,0 +1,14 @@
+package iopackage;
+
+import java.io.*;
+
+public class FileOutputStreamDemo {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream filestream = new FileOutputStream("file.txt");
+        String stm = "Hello World!";
+        filestream.write(stm.getBytes());
+        System.out.println("File written successfully");
+        filestream.close();
+        
+    }
+}
