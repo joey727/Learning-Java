e9c9e9c37b04b46df4459a1ba4b59e7243870bb0
Joey727 <joshuasackeyjr.1124@gmail.com>
2025-01-16 18:38:32 +0000

    mistake fixed whereas there wasn't

--- JavaScript Refresh/create.js
+++ JavaScript Refresh/create.js
@@ -1,2 +1,18 @@
-const car = {"name:" "Ford", "model": "Mustang", "year": 1964};
+// using object literals syntax
+const car = {"name": "Ford", "model": "Mustang", "year": 1964};
 console.log(car.name);
+
+// using new keyword
+const person = new Object();
+person.firstName = "Joey";
+person.lastName = "Khalifa";
+person.age = 25;
+person.eyeColor = "brown";
+console.log(person); 
+
+// using constructor function 
+function Car(name, model, year) {
+    this.name = name;
+    this.model = model;
+    this.year = year;
+}
